0405e244d004639c5d331808dbdb20ec88d4ed29
sampathNG <jnanisampath@gmail.com>
2024-08-07 16:33:58 +0530

    add

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True,timeout_keep_alive=60)
 # command to start app
 # uvicorn main:app  --reload
+# postman collection
+# https://documenter.getpostman.com/view/16226090/2sA3rzKYJ1
